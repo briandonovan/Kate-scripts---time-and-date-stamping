7cb43ff641cc82a04728f9ca9ca5ec25fd61860e
Brian Donovan <brian@briandon.com>
2017-03-29 01:39:33 +0800

    Working! Purring like a kitten, too!

--- time-and-date.js
+++ time-and-date.js
@@ -3,25 +3,33 @@ var katescript = {
     "license": "BSD",
     "revision": 1,
     "kate-version": "0",
-    "functions": ["insertDatestamp", "insertDateTimestamp"],
+    "functions": ["insertYYMMDDdatestamp", "insertYYMMDDhhmmdatestamp", "insertDatestamp", "insertDateTimestamp"],
     "actions": [
+        {   "function": "insertYYMMDDdatestamp",
+            "name": "Insert YYMMDD current date",
+            "category": "Brian's Utility Belt"
+        },
+        {   "function": "insertYYMMDDhhmmdatestamp",
+            "name": "Insert YYMMDD hh:mm current date and time",
+            "shortcut": "Ctrl+Alt+d",
+            "category": "Brian's Utility Belt"
+        },
         {   "function": "insertDatestamp",
             "name": "Insert the current date",
             "category": "Brian's Utility Belt"
-        },
+        }
+,
         {   "function": "insertDateTimestamp",
             "name": "Insert the current date and time",
+            "shortcut": "Ctrl+Alt+t",
             "category": "Brian's Utility Belt"
         }
-
     ]
-}; // kate-script-header, must be at the start of the file without comments, pure json
+};
 
 require ("range.js");
-require ("documentcursor.js");
 
-function insertDatestamp(){
-    debug('insertDatestamp called!');
+function insertYYMMDDdatestamp(){
     var objDateTime = new Date(Date.now());
     var str4digitYear = objDateTime.getUTCFullYear().toString();
     var str2digitMonth = objDateTime.getUTCMonth().toString();
@@ -33,19 +41,55 @@ function insertDatestamp(){
         str2digitDate = '0' + str2digitDate;
     }
     var strDatestamp = str4digitYear+str2digitMonth+str2digitDate;
+    document.editBegin();
+    document.insertText(view.cursorPosition().line, view.cursorPosition().column, strDatestamp);
+    document.editEnd();
+    return true;
+}
+
+function insertYYMMDDhhmmdatestamp(){
+    var objDateTime = new Date(Date.now());
+    var str4digitYear = objDateTime.getUTCFullYear().toString();
+    var str2digitMonth = objDateTime.getUTCMonth().toString();
+    if (str2digitMonth.length === 1) {
+        str2digitMonth = '0' + str2digitMonth;
+    }
+    var str2digitDate = objDateTime.getUTCDate().toString();
+    if (str2digitDate.length === 1) {
+        str2digitDate = '0' + str2digitDate;
+    }
+    var strDatestamp = str4digitYear+str2digitMonth+str2digitDate;
+    var str2digitHours = objDateTime.getUTCHours().toString();
+    if (str2digitHours.length === 1) {
+        str2digitHours = '0' + str2digitHours;
+    }
+    var str2digitMinutes = objDateTime.getUTCMinutes().toString();
+    if (str2digitMinutes.length === 1) {
+        str2digitMinutes = '0' + str2digitMinutes;
+    }
+    var strDatestampWithTime = strDatestamp+' '+str2digitHours+':'+str2digitMinutes;
+    document.editBegin();
+    document.insertText(view.cursorPosition().line, view.cursorPosition().column, strDatestampWithTime);
+    document.editEnd();
+    return true;
+}
+
+function insertDatestamp(){
+    var objDateTime = new Date(Date.now());
+    var strDate = objDateTime.toLocaleDateString();
+    document.editBegin();
+    document.insertText(view.cursorPosition().line, view.cursorPosition().column, strDate);
+    document.editEnd();
     return true;
 }
 
 function insertDateTimestamp(){
-    //debug('insertDateTimestamp called!');
     var objDateTime = new Date(Date.now());
-    var strDateTimestamp = objDateTime.toLocaleDateString();
-    //debug(strDateTimestamp);
-    //debug(view.cursorPosition().line);
-    //debug(view.cursorPosition().column);
+    var strDate = objDateTime.toLocaleDateString();
+    var strTime = objDateTime.toLocaleTimeString();
+    var strDateTimestamp = strDate+' '+strTime;
     document.editBegin();
-    document.insertText((view.cursorPosition().line, view.cursorPosition().column), strDateTimestamp);
-    //alert(strDateTimestamp);
+    document.insertText(view.cursorPosition().line, view.cursorPosition().column, strDateTimestamp);
     document.editEnd();
     return true;
 }
