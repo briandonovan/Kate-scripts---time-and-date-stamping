dc3712261feaf594c8458760d8f9b2c64ba22650
Brian Donovan <brian@briandon.com>
2017-04-12 22:45:25 +0800

    Tidied this script up a bit.

--- time-and-date.js
+++ time-and-date.js
@@ -5,32 +5,26 @@ var katescript = {
     "kate-version": "0",
     "functions": ["insertYYMMDDdatestamp", "insertYYMMDDhhmmdatestamp", "insertDatestamp", "insertDateTimestamp"],
     "actions": [
-        {   "function": "insertYYMMDDdatestamp",
+        {"function": "insertYYMMDDdatestamp",
             "name": "Insert YYMMDD current date",
-            "category": "Brian's Utility Belt"
-        },
-        {   "function": "insertYYMMDDhhmmdatestamp",
+            "category": "Brian's Utility Belt"},
+        {"function": "insertYYMMDDhhmmdatestamp",
             "name": "Insert YYMMDD hh:mm current date and time",
             "shortcut": "Ctrl+Alt+d",
-            "category": "Brian's Utility Belt"
-        },
-        {   "function": "insertDatestamp",
+            "category": "Brian's Utility Belt"},
+        {"function": "insertDatestamp",
             "name": "Insert the current date",
-            "category": "Brian's Utility Belt"
-        }
-,
-        {   "function": "insertDateTimestamp",
+            "category": "Brian's Utility Belt"},
+        {"function": "insertDateTimestamp",
             "name": "Insert the current date and time",
             "shortcut": "Ctrl+Alt+t",
-            "category": "Brian's Utility Belt"
-        }
+            "category": "Brian's Utility Belt"}
     ]
 };
 
 require ("range.js");
 
-function insertYYMMDDdatestamp(){
-    var objDateTime = new Date(Date.now());
+function rtnStrYYMMDDdatestamp(objDateTime){
     var str4digitYear = objDateTime.getUTCFullYear().toString();
     var str2digitMonth = objDateTime.getUTCMonth().toString();
     if (str2digitMonth.length === 1) {
@@ -41,24 +35,19 @@ function insertYYMMDDdatestamp(){
         str2digitDate = '0' + str2digitDate;
     }
     var strDatestamp = str4digitYear+str2digitMonth+str2digitDate;
+    return strDatestamp;
+}
+
+function insertYYMMDDdatestamp(){
+    var objDateTime = new Date(Date.now());
+    var strDatestamp = rtnStrYYMMDDdatestamp(objDateTime);
     document.editBegin();
     document.insertText(view.cursorPosition().line, view.cursorPosition().column, strDatestamp);
     document.editEnd();
     return true;
 }
 
-function insertYYMMDDhhmmdatestamp(){
-    var objDateTime = new Date(Date.now());
-    var str4digitYear = objDateTime.getUTCFullYear().toString();
-    var str2digitMonth = objDateTime.getUTCMonth().toString();
-    if (str2digitMonth.length === 1) {
-        str2digitMonth = '0' + str2digitMonth;
-    }
-    var str2digitDate = objDateTime.getUTCDate().toString();
-    if (str2digitDate.length === 1) {
-        str2digitDate = '0' + str2digitDate;
-    }
-    var strDatestamp = str4digitYear+str2digitMonth+str2digitDate;
+function rtnStrHHMMTimestamp(objDateTime){
     var str2digitHours = objDateTime.getUTCHours().toString();
     if (str2digitHours.length === 1) {
         str2digitHours = '0' + str2digitHours;
@@ -67,7 +56,15 @@ function insertYYMMDDhhmmdatestamp(){
     if (str2digitMinutes.length === 1) {
         str2digitMinutes = '0' + str2digitMinutes;
     }
-    var strDatestampWithTime = strDatestamp+' '+str2digitHours+':'+str2digitMinutes;
+    var strTimestamp = str2digitHours+':'+str2digitMinutes;
+    return strTimestamp;
+}
+
+function insertYYMMDDhhmmdatestamp(){
+    var objDateTime = new Date(Date.now());
+    var strDatestamp = rtnStrYYMMDDdatestamp(objDateTime);
+    var strTimestamp = rtnStrHHMMTimestamp(objDateTime);
+    var strDatestampWithTime = strDatestamp+' '+strTimestamp;
     document.editBegin();
     document.insertText(view.cursorPosition().line, view.cursorPosition().column, strDatestampWithTime);
     document.editEnd();
